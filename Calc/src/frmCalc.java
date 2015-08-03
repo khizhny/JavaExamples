7b7232b67b7921a7881918df352f48c75af63130
Andrey.Khizhny <khizhny@gmail.com>
2015-08-03 14:11:26 +0300

    Third

--- Calc/src/frmCalc.java
+++ Calc/src/frmCalc.java
@@ -1,11 +1,13 @@
 import java.awt.EventQueue;
-import javax.swing.JFrame;
-import javax.swing.JButton;
-import net.miginfocom.swing.MigLayout;
-import javax.swing.JTextField;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+import net.miginfocom.swing.MigLayout;
 
 public class frmCalc {
 
